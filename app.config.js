6db370c7d63b42bc03b2d0faac3329c122809390
BasitKhokhar <127882915+BasitKhokhar@users.noreply.github.com>
2025-06-15 18:58:22 +0500

    new changes

--- app.config.js
+++ app.config.js
@@ -2,11 +2,15 @@ import 'dotenv/config';
 export default () => ({
   expo: {
     name: "Basit Sanitary App",
-    slug: "your-app-slug",
+    slug: "basit-sanitary-app",
+    owner: "basitkhokhar4949",
     version: "1.0.0",
     extra: {
       stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
       API_BASE_URL: process.env.API_BASE_URL,
+    },
+    android: {
+      "package": "com.basitkhokhar.sanitaryapp"
     },
     plugins: [
       "expo-secure-store",
