9eeb686d7a66f5bf5d8508f1b06787b94b53adf0
BasitKhokhar <127882915+BasitKhokhar@users.noreply.github.com>
2025-07-05 12:34:00 +0500

    changes

--- app.config.js
+++ app.config.js
@@ -30,7 +30,7 @@ export default () => ({
       stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
       API_BASE_URL: process.env.API_BASE_URL,
       eas: {
-        projectId: "00419d90-2f51-4972-b963-c5d1daad4b19" // ✅ required for linking to EAS
+        projectId: "00419d90-2f51-4972-b963-c5d1daad4b19" 
       },
 
     }
